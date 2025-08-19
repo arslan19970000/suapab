0e7201a127b87b00fb01834b66606c0bc1465511
arslan rana <arslanbashir746r@gmail.com>
2025-08-19 16:01:49 +0500

    Fix Stripe initialization by removing apiVersion

--- app/api/checkout/success/route.ts
+++ app/api/checkout/success/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server'
 import Stripe from 'stripe'
 import { supabaseAdmin } from '@/lib/supabaseAdmin'
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: '2024-06-20' })
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
 
 
 
